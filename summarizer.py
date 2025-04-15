fc84c11cd3d4466eebd4a4a3191b552cce248008
Mastercode-Lunarbyte <pejman1forouzan@yahoo.com>
2025-04-15 10:34:48 +0330

    summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -6,13 +6,12 @@
 
 # summarizer.py
 
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer  # می‌شود از سایر الگوریتم‌ها هم استفاده کرد
+# summarizer.py
 
-def summarize_text(text, num_sentences=3):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, num_sentences)
-    return ' '.join(str(sentence) for sentence in summary)
+def summarize_text(text):
+    if len(text) <= 300:
+        return text
+    else:
+        return text[:300] + "..."
+
 
