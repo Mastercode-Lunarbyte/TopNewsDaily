255d091df30600a2896145a7f600a017027388f8
Mastercode-Lunarbyte <pejman1forouzan@yahoo.com>
2025-04-18 11:58:52 +0330

    summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,16 +1,19 @@
 from transformers import pipeline
 
-# بارگذاری تنها یک‌بار هنگام اجرای سرور
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+# مدل سبک‌تر T5
+summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small")
 
-def summarize_text(text, max_length=130, min_length=30):
+def summarize_text(text, max_length=100, min_length=30):
     try:
         if len(text) < 100:
             return text
+
+        # برش متن به 512 کاراکتر، چون t5-small محدودیت داره
+        text = text[:512]
 
         summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
         return summary[0]['summary_text']
     
     except Exception as e:
         print(f"❌ خطا در خلاصه‌سازی متن: {e}")
-        return text[:1000] + "..."
+        return text[:300] + "..."
